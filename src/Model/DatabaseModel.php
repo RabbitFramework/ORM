961cc455985c0e63ec9246151a346d7a2e41b330
Kakktuss <benjaminmanderveldepro@gmail.com>
2018-12-23 19:57:17 +0100

    [Add] MVC completion

---
+++ src/Model/DatabaseModel.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 23/12/2018
+ * Time: 19:39
+ */
+
+namespace Rabbit\ORM\Model;
+
+use Rabbit\ORM\Database;
+
+if(class_exists('\Rabbit\MVC\Model')) {
+    class DatabaseModel implements Rabbit\MVC\ModelInterfac
+    {
+        protected $driver;
+        protected $builder;
+
+        public function __construct()
+        {
+            $this->driver = Database::getInstance()->getDriver();
+            $this->builder = $this->driver->getBuilder();
+        }
+    }
+}
